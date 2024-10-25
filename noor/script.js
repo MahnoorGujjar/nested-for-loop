fd38e627ef50f1eaefea5ed4e55aba55b017f5e1
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-10-25 16:52:10 +0500

    Add files via upload

---
+++ noor/script.js
@@ -0,0 +1,95 @@
+// NESTED FOR LOOP
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     for (let j = 1 ; j <= 5 ; j++){
+//         document.write(i , j, "</br>")
+//     }
+// }
+
+
+ // 11 12 13 14 15 21 22 23 24 25  31 32 33 34 35................. 105 
+
+
+ // outer foor loop inner for loop pe depend krta he its mean k inner loop 5 tk chlega then jse hi hmri condtion false
+ // hgi tb outer for loop 1 se 2 hga
+
+
+                 //////////////................../////////////////.....................///////////////////
+
+
+
+
+// PATERN THROUGH NESTED FOR LOOP
+
+
+// for (let k= 1 ; k <= 5 ; k++){
+//     for (let m = 1 ; m <= k ; m++){
+//         document.write(k)
+//     }
+    
+// document.write("<br>")
+// }
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+
+
+                 //////////////................../////////////////...................../////////////////// 
+
+
+
+// for (let k= 1 ; k <= 5 ; k++){
+//     for (let m = 1 ; m <= k ; m++){
+//         document.write(m)
+//     }
+    
+// document.write("<br>")
+// }
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+
+
+                 //////////////................../////////////////...................../////////////////// 
+
+
+
+
+// for (let i = 5 ; i >= 1; i--){
+//     for (let j =i ; j >= 1 ; j--){
+//         document.write(i)
+//     }
+//     document.write("<br>")
+// }
+
+
+// 55555
+// 4444
+// 333
+// 22
+// 1
+
+
+
+                 //////////////................../////////////////...................../////////////////// 
+
+
+
+// let firsName = ["BlueRay" , "Upchuck" , "Lojack", "Gizmo" , "Do-Rag"];
+// let lastName = ["Zzz", "Burp" , "Dogbone" , "Droop"]
+// let fullName = []
+
+// for (let i = 0 ; i < firsName.length ; i++){
+//     for (let j = 0 ; j < lastName.length ; j++){
+//         fullName.push(firsName[i] + lastName[j])
+//     }
+//     document.write(fullName[i], "<br>")
+}
